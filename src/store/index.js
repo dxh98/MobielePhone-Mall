792bd87250f47f138b769469a79fefb0fda79cdc
weimingliang <18438627291@163.com>
2020-06-15 19:36:58 +0800

    不知道啥

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import Vant from 'vant';
-import 'vant/lib/index.css';
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import Vant from "vant";
+import "vant/lib/index.css";
+Vue.use(Vuex);
 Vue.use(Vant);
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
-})
+  modules: {},
+});
