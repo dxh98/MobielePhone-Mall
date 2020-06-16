6bde308e37cb2989cb66a712a852db9f5b1b8c3c
dxh98 <787805458@qq.com>
2020-06-16 15:29:27 +0800

    完成了购物车页面的静态构建

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import Vant from 'vant'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'vant/lib/index.css'
 require('./assets/css/reset.css')
+
+Vue.use(Vant)
 
 
 Vue.config.productionTip = false
